66ac85bbd3aa748612d0eea2f96248a202026e1b
vonwao <vonwao@gmail.com>
2021-06-07 14:40:19 -0700

    test kucoin api buy and sell

--- app.js
+++ app.js
@@ -9,30 +9,46 @@
  * which can be found here: https://github.com/jaggedsoft/node-binance-api
  * Thanks Jon!
  *****************************************************************************/
-const Binance = require( 'node-binance-api' );
+// const Binance = require( 'node-binance-api' );
 const http = require('http');
 const events = require('events');
 require('dotenv').config();
 
 // B i n a n c e - N o d e - A P I   O p t i o n s
-const binance = new Binance().options({
-  APIKEY: process.env.BINANCE_APIKEY,
-  APISECRET: process.env.BINANCE_SECRET,
-  useServerTime: true,
-  recvWindow: 1500, // Set a higher recvWindow to increase response timeout
-  verbose: false, // Add extra output when subscribing to WebSockets, etc
-  test: false,
-  reconnect: true
-  // to do: enable Logging
-});
-
-const symbol = 'BTCUSDT';
-const quantity = 0.0016;
+// const binance = new Binance().options({
+//   APIKEY: process.env.BINANCE_APIKEY,
+//   APISECRET: process.env.BINANCE_SECRET,
+//   useServerTime: true,
+//   recvWindow: 1500, // Set a higher recvWindow to increase response timeout
+//   verbose: false, // Add extra output when subscribing to WebSockets, etc
+//   test: false,
+//   reconnect: true
+//   // to do: enable Logging
+// });
+// const symbol = 'BTCUSDT';
+// const quantity = 0.0016;
 const hostname = '127.0.0.1';
-const port = 80;
+const port = 8000;
 
 // Are we in test mode?
-console.log ("Test Mode: ", binance.getOption('test'));
+// console.log ("Test Mode: ", binance.getOption('test'));
+
+
+// KUCOIN
+const ccxt = require ('ccxt');
+let kucoin    = new ccxt.kucoin ({
+      password: process.env.KUCOIN_PASSWORD,
+      apiKey: process.env.KUCOIN_APIKEY,
+      secret: process.env.KUCOIN_SECRET,
+  })
+
+  // const trylimit = async () => {
+  //   console.log('try limit order')
+  // const order = await kucoin.createOrder ('SUSHI/USDT', 'limit', 'buy', 1, 8)
+  // console.log (order)
+  // }
+
+  // trylimit()
 
 var eventEmitter = new events.EventEmitter();
 
@@ -40,55 +56,37 @@ eventEmitter.on('error', (err) => {
   console.error(err);
 })
 
-eventEmitter.on('buy', () => {
+eventEmitter.on('buy', async () => {
 
-  binance.balance((error, balances) => {
-    if ( error ) return console.error(error);
-    const usdtBal = balances.USDT.available;
-    if (balances.USDT.available > 10.00) {
-      binance.bookTickers('BTCUSDT', (error, ticker) => {
-        tickAsk = ticker.askPrice;
-        let qty = usdtBal/tickAsk;
-        qty = qty.toFixed(5);
-        console.log(tickAsk, usdtBal, qty);
-        binance.marketBuy(symbol, qty);
-      });
-    }
-    else {
-      console.log('Balance < 20.00')
-    }
-  }) // binance.balance
+  const symbol = 'SUSHI/USDT'
+  const amount = 1
+  const price = 10.89 // USD
+  // cost = amount * price = 2 * 9000 = 18000 (USD)
+
+  // note that we don't use createMarketBuyOrder here, instead we use createOrder
+  // createMarketBuyOrder will omit the price and will not work when
+  // exchange.options['createMarketBuyOrderRequiresPrice'] = true
+  const order = await kucoin.createOrder (symbol, 'market', 'buy', amount, price)
+
+  console.log (order)
+
 }) // eventemitter.on('buy')
 
-eventEmitter.on('sell', () => {
+eventEmitter.on('sell', async () => {
 
-  binance.balance((error, balances) => {
-    if ( error ) return console.error(error);
-    
-    if ( balances.BTC.available > quantity ) {
-      binance.marketSell(symbol, quantity);
-    }
-  
-    else {
-      
-      binance.marketSell(symbol, balances.BTC.available);
-    }
-    console.log(balances.BTC.available);
-  }) // binance.balance
+  const symbol = 'SUSHI/USDT'
+  const amount = 1
+  const price = 10.89 // USD
+  const order = await kucoin.createOrder (symbol, 'market', 'sell', amount, price)
+  console.log (order)
+
 }) // end eventemitter.on('sell')
 
 //  S T O P
 eventEmitter.on('stop', () => {
   
-  binance.balance((error, balances) => {
-    if ( error ) return console.error(error);
-
-    if( balances.BTC.available > 0) {
-      btcBalance = parseFloat(balances.BTC.available);
-      console.log(btcBalance);
-      binance.marketSell(symbol, btcBalance);
-    }
-  }) // binance.balance
+  console.log ('no stop handling yet')
+  
   
 }) // end eventemitter.on('stop')
 
@@ -104,10 +102,12 @@ const server = http.createServer((req, res) => {
 
     if(body === 'buy') { 
       eventEmitter.emit('buy'); // <----------------------- BUY
+      console.log('--- buy triggered');
     } 
     
     if(body === 'sell') {
       eventEmitter.emit('sell'); // <---------------------- SELL
+      console.log('--- sell triggered');
     }
     
     if(body === 'stop') {
